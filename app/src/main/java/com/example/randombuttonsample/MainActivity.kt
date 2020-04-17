0ed6effcbf0329f50b886509e80c78f59e09a3c5
Bob Fly <d.a.kryl@gmail.com>
2020-04-17 20:00:18 +0300

    Init Prg

---
+++ app/src/main/java/com/example/randombuttonsample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.randombuttonsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        randomButton.setOnClickListener {
+            val sRange = editStart.text.toString().toInt()
+            val eRange = editEnd.text.toString().toInt()
+            val randomDigit=(sRange .. eRange).random()
+            txtForRandom.text="$randomDigit"
+        }
+    }
+}
